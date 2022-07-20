47a745c36cf2ec6a11315982f1a2e392bf76fa65
Anabella <shokoufeh.namazian@gmail.com>
2022-07-20 14:04:55 -0700

    en

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,2 +1,5 @@
-// export const baseUrl = 'http://localhost:3001/';
-export const baseUrl = 'https://localhost:3443/';
+//export const baseUrl = 'http://localhost:3001/';
+ //export const baseUrl = 'https://localhost:3443/';
+
+
+export const baseUrl  = process.env.REACT_APP_BASE_URL;

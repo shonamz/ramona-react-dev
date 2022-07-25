ff8efff7b964d7a4da92ccfa967f3a08fc147872
shonamz <shokoufeh.namazian@gmail.com>
2022-07-24 19:27:01 -0700

    env fixed

--- src/shared/baseUrl 2.js
+++
@@ -1,6 +0,0 @@
-//export const baseUrl = 'http://localhost:3001/';
-export const baseUrl = 'https://localhost:3443/';
-
-//  require('dotenv').config()
-//  console.log(process.env) 
- //export const baseUrl  = process.env.REACT_APP_BASE_URL;
